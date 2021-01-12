a7c87e8b3804a378caa2f1a379724e37baa85430
redbean88 <doctortor88@gmail.com>
2021-01-12 23:35:45 +0900

    라우터

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [
+    'as' => 'home',
+    function () {
+    return '메인 화면 입니다';
+    }
+]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function(){
+    return redirect(route('home'));
 });
